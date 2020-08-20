089eea527c97dd97f0cf7c9090773bbac040e16f
ksh3760 <ksh3760@naver.com>
2020-08-20 22:15:08 +0900

    update

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ var yDeg             = 0,                                   // 각도
     w                = page[0].offsetWidth,                 // 현재 페이지 폭
     page_angle       = 0;                                   // 각 페이지 4면체의 배치를 위한 각도를 정의
 
+var hammer = new hammer(wrapper);
 
 function init_page(){
     w = page[0].offsetWidth;
@@ -23,4 +24,65 @@ function init_page(){
     wrapper.style.transform = 'translateZ(' + (-w/2) + 'px) rotaateY(' + yDeg + 'deg)';
 }
 
+// 인디케이터 초기화
+function init_indicator(){
+    // 인디케이터 표시
+    for(var i = 0 ; i < indicator_length ; i++){
+        indicator.innerHTML += '<li>' + (i+1) + '</li>';
+    }
+    indicator_li = indicator.querySelectorAll('li');
+    change_page(indicator_num);
+}
+
+// 페이지 전환
+function change_page(inum){
+    indicator_li[inum-1].setAttribute('class', 'active');
+    yDeeg = -90 * (inum - 1);
+    wrapper.style.transform = 'translateZ(' + (-w/2) + ' px) rotateY(' + yDeg + 'deg)';
+
+    // 인디케이터 표시
+    for(var i = 0 ; i < indicator_li.length ; i++){
+        indicator_li[i].removeAttribute('class');
+    }
+    indicator_li[inum - 1].setAttribute('class', 'active');
+}
+
 init_page();
+init_indicator();
+
+for(var i = 0 ; i < indicator_li.length ; i++){
+    indicator_li[i].addEventListener('click', function(){
+        indicator_num = parseInt(this.innerText);
+        change_page(indicator_num);
+    });
+}
+
+// 터치 swipe left
+hammer.on('swipeleft', function(e){
+    // 인디케이터(페이지) 이동 범위 내이면
+    if(indicator_num < indicator_length){
+        page_vector = 1;
+    } else {
+        page_vector = 0;
+    }
+
+    indicator_num += page_vector;
+    change_page(indicator_num);
+});
+
+// 터치 swipe right
+hammer.on('swiperight', function(e){
+    if(indicator_num > 1){
+        page_vector = -1;
+    } else {
+        page_vector = 0;
+    }
+
+    indicator_num += page_vector;
+    change_page(indicator_num);
+});
+
+// 창 크기 변경 시 페이지 초기화
+window.onresize = function(){
+    init_page();
+}
